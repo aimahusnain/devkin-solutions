d52b5dae17973d70d05f8c66c608af3addcab1bc
aimahusnain <aimahusnain@gmail.com>
2023-12-05 19:00:58 +0500

    Take Links from Blog

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,21 @@
 import Link from "next/link";
 
 export default function Home() {
-
   return (
     <main className="">
-      <Link href='/blog'>Blog</Link>
+      <div className="flex flex-col gap-4">
+        <ul className="">
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </div>
     </main>
   );
 }
