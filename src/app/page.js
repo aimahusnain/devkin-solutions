ce1e0335e6cdeda1eee16349d3f74b4f5752c0c6
aimahusnain <aimahusnain@gmail.com>
2023-12-05 18:47:33 +0500

    Blog

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,10 @@
-// import AboutUs from "@/src/components/AboutUs";
-// import Contact from "@/src/components/Contact";
-// import HomeBanner from "@/src/components/HomeBanner";
-// import Services from "@/src/components/Services";
-
-// const Portfolio = dynamic(() => import("@/src/components/Portfolio"), {
-//   ssr: false,
-// });
+import Link from "next/link";
 
 export default function Home() {
 
   return (
     <main className="">
-ffdff
+      <Link href='/blog'>Blog</Link>
     </main>
   );
 }
