7f9c5e2d41dffcca82f382dd587f36360280e383
aimahusnain <aimahusnain@gmail.com>
2023-12-05 18:37:48 +0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,17 @@
+// import AboutUs from "@/src/components/AboutUs";
+// import Contact from "@/src/components/Contact";
+// import HomeBanner from "@/src/components/HomeBanner";
+// import Services from "@/src/components/Services";
+
+// const Portfolio = dynamic(() => import("@/src/components/Portfolio"), {
+//   ssr: false,
+// });
+
+export default function Home() {
+
+  return (
+    <main className="">
+ffdff
+    </main>
+  );
+}
